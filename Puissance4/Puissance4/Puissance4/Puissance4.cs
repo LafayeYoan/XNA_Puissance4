6819f96fba516465d2a63a3b94eba1160727f2dc
Sacha Lhopital <sachalhopital5@gmail.com>
2015-12-16 11:45:54 +0100

    Display cursor selector for player turn

--- Puissance4/Puissance4/Puissance4/Puissance4.cs
+++ Puissance4/Puissance4/Puissance4/Puissance4.cs
@@ -19,6 +19,8 @@ namespace Puissance4
         public const int NB_COLONNES = 7;
         public const int NB_LIGNES = 6;
         public const int TAILLE_BLOCK = 80;
+        public const int OFFSET_X = 140;
+        public const int OFFSET_Y = 140;
 
         // 0 : case libre
         // 1 : case occupée par le joueur
@@ -27,10 +29,12 @@ namespace Puissance4
         private String direction;
 
         private Boolean isJoueurTurn;
+        private Boolean lockKey;
         //cursorPosition représente le curseur du joueur pour choisir où placer son pion
         private int cursorPosition;
 
         private ObjetPuissance4 cadre;
+        private ObjetPuissance4 cursor;
         private ObjetPuissance4 pion_IA;
         private ObjetPuissance4 pion_Joueur;
 
@@ -40,6 +44,7 @@ namespace Puissance4
         public Puissance4()
         {
             isJoueurTurn = true;
+            lockKey = false;
             cursorPosition = 0;
 
             map = new int[NB_LIGNES, NB_COLONNES]{
@@ -81,6 +86,7 @@ namespace Puissance4
             cadre = new ObjetPuissance4(Content.Load<Texture2D>("Images\\cadre"), new Vector2(0f, 0f), new Vector2(TAILLE_BLOCK, TAILLE_BLOCK));
             pion_IA = new ObjetPuissance4(Content.Load<Texture2D>("Images\\pion_IA"), new Vector2(0f, 0f), new Vector2(TAILLE_BLOCK, TAILLE_BLOCK));
             pion_Joueur = new ObjetPuissance4(Content.Load<Texture2D>("Images\\pion_Joueur"), new Vector2(0f, 0f), new Vector2(TAILLE_BLOCK, TAILLE_BLOCK));
+            cursor = new ObjetPuissance4(Content.Load<Texture2D>("Images\\cursor"), new Vector2(0f, 0f), new Vector2(TAILLE_BLOCK, TAILLE_BLOCK));
         }
 
         /// <summary>
@@ -106,15 +112,16 @@ namespace Puissance4
             if (isJoueurTurn == true)
             {
                 KeyboardState keyboard = Keyboard.GetState();
-                if (keyboard.IsKeyDown(Keys.Right))
+                if (keyboard.IsKeyDown(Keys.Right) && !lockKey)
                 {
                     direction = "Right";
                     if (cursorPosition < (NB_COLONNES - 1))
                     {
                         cursorPosition ++;
-                    } 
+                    }
+                    lockKey = true;
                 }
-                if (keyboard.IsKeyDown(Keys.Left))
+                if (keyboard.IsKeyDown(Keys.Left) && !lockKey)
                 {
                     //TODO : vérifier que la variable direction sert réellement à quelque chose ?
                     direction = "Left";
@@ -122,14 +129,33 @@ namespace Puissance4
                     {
                         cursorPosition--;
                     }
+                    lockKey = true;
                 }
-                if (keyboard.IsKeyDown(Keys.Down))
+                if (keyboard.IsKeyDown(Keys.Down) && !lockKey)
                 {
                     direction = "Down";
                     //TODO : descendre le pion au bon endroit dans la matrice
                     //TODO : Transparence du curseur
                     isJoueurTurn = false;
+                    lockKey = true;
                 }
+
+                switch (direction)
+                {
+                    case("Right"):
+                        if (keyboard.IsKeyUp(Keys.Right))
+                            lockKey = false;
+                        break;
+                    case ("Left"):
+                        if (keyboard.IsKeyUp(Keys.Left))
+                            lockKey = false;
+                        break;
+                    case ("Down"):
+                        if (keyboard.IsKeyUp(Keys.Down))
+                            lockKey = false;
+                        break;
+                }
+                
             }
             else
             {
@@ -137,20 +163,26 @@ namespace Puissance4
                 isJoueurTurn = true;
             }
 
-
+            
             //TODO : Mettre à jour l'affichage
             base.Update(gameTime);
         }
 
         private void DrawCursor(){
-
+            
+            int xpos, ypos;
+            xpos = OFFSET_X - TAILLE_BLOCK;
+            ypos = OFFSET_Y + cursorPosition * TAILLE_BLOCK;
+            Vector2 pos = new Vector2(ypos, xpos);
+            spriteBatch.Draw(cursor.Texture, pos, Color.White);
+            
         }
 
-        private void DrawBlock(ObjetPuissance4 obj, int offsetX, int offsetY, int x, int y)
+        private void DrawBlock(ObjetPuissance4 obj, int x, int y)
         {
             int xpos, ypos;
-            xpos = offsetX + x * TAILLE_BLOCK;
-            ypos = offsetY + y * TAILLE_BLOCK;
+            xpos = OFFSET_X + x * TAILLE_BLOCK;
+            ypos = OFFSET_Y + y * TAILLE_BLOCK;
             Vector2 pos = new Vector2(ypos, xpos);
             spriteBatch.Draw(obj.Texture, pos, Color.White);
         }
@@ -164,29 +196,27 @@ namespace Puissance4
             GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin();
-
-            int offsetX = 140;
-            int offsetY = 140;
             for (int x = 0; x < NB_LIGNES; x++)
             {
                 for (int y = 0; y < NB_COLONNES; y++)
                 {
                     if (map[x, y] == 0)
                     {
-                        DrawBlock(cadre, offsetX, offsetY, x, y);
+                        DrawBlock(cadre,  x, y);
                     }
 
                     if (map[x, y] == 1)
                     {
-                        DrawBlock(pion_Joueur, offsetX, offsetY, x, y);
+                        DrawBlock(pion_Joueur, x, y);
                     }
 
                     if (map[x, y] == 2)
                     {
-                        DrawBlock(pion_IA, offsetX, offsetY, x, y);
+                        DrawBlock(pion_IA,  x, y);
                     }
                 }
             }
+            DrawCursor();
             spriteBatch.End();
             base.Draw(gameTime);
         }
